a33df5b8637d552cd376dab3e32128908f4dbac9
Bofrostmann07 <84816381+Bofrostmann07@users.noreply.github.com>
2022-01-20 23:58:51 +0100

    feat: added multithreading, reworked data flow
    close #16. close #7

--- ssh_connection.py
+++ ssh_connection.py
@@ -1,6 +1,5 @@
 import logging
 import json
-import re
 from netmiko import ConnectHandler
 from netmiko.ssh_exception import NetMikoTimeoutException
 from paramiko.ssh_exception import SSHException
@@ -11,11 +10,15 @@ from alive_progress import alive_bar
 from threading import Event, Thread
 from queue import Queue, Empty
 from time import sleep
+from datetime import datetime
+
 
 logging.getLogger("paramiko.transport").setLevel(logging.WARNING)
 logging.getLogger("netmiko").setLevel(logging.WARNING)
 
+tool_config = None
 worker_threads = []
+
 
 # need to get rid of ot that function, as soon as multithreading code is reliable
 def ssh_connect_only_one_show_command_singlethreaded(switch_data, cli_show_command, global_config):
@@ -48,47 +51,25 @@ def ssh_connect_only_one_show_command_singlethreaded(switch_data, cli_show_comma
     return output_data
 
 
-
-
-
-
-
-
-
 def start_workers(num_workers, bar):
     stop_event = Event()
     input_queue = Queue()
     output_queue = Queue()
-    for i in range(num_workers):
-        w = Thread(target=worker, args=(i, stop_event, input_queue, output_queue, bar))
+    for _ in range(num_workers):
+        w = Thread(target=worker, args=(stop_event, input_queue, output_queue, bar))
         w.daemon = True
         w.start()
         worker_threads.append(w)
     return stop_event, input_queue, output_queue
 
 
-def construct_ssh_parameter(switch_data, global_config):
-    ssh_parameter_list = []
-    for switch_element in switch_data:
-        if switch_element.reachable is True:
-            ssh_parameter = {
-                'device_type': switch_element.os,
-                'host': switch_element.ip,
-                'username': global_config.ssh_username,
-                'password': global_config.ssh_password,
-                'port': global_config.ssh_port
-            }
-            ssh_parameter_list.append(ssh_parameter)
-    return ssh_parameter_list
-
-
-def fill_input_queue_start_worker_fill_output_queue(ssh_parameter_list, cli_show_command, global_config):
-    logging.info(f"Starting to execute CLI commands for {len(ssh_parameter_list)} switches...")
-    with alive_bar(total=len(ssh_parameter_list)) as bar:
+def fill_input_queue_start_worker_fill_output_queue(switches, cli_show_command, global_config):
+    logging.info(f"Starting to execute '{cli_show_command}' on {len(switches)} switches...")
+    with alive_bar(total=len(switches)) as bar:
         stop_event, input_queue, output_queue = start_workers(num_workers=global_config.number_of_worker_threads, bar=bar)
-        for ssh_parameter in ssh_parameter_list:
-            command = "show derived-config | begin interface"
-            input_queue.put((ssh_parameter, command))
+
+        for switch_element in switches:
+            input_queue.put((switch_element, cli_show_command))
 
         # Wait for the input queue to be emptied
         while not input_queue.empty():
@@ -101,85 +82,81 @@ def fill_input_queue_start_worker_fill_output_queue(ssh_parameter_list, cli_show
 
     # Wait for the output queue to be emptied
     combined_cli_output = {}
-    while not output_queue.empty() and len(ssh_parameter_list) != len(combined_cli_output):
-    # while not output_queue.empty():
+    while not output_queue.empty() and len(switches) != len(combined_cli_output):
         try:
-            cli_output = output_queue.get(block=False)
+            switch_element = output_queue.get(block=False)
         except Empty:
             pass
         else:
-            combined_cli_output.update(cli_output)
+            combined_cli_output.update(switch_element.config_to_dict())
     logging.debug(json.dumps(combined_cli_output))
     return combined_cli_output
 
 
-def worker(i, stop_event, input_queue, output_queue, bar):
+def fetch_switch_config(switch_element, command):
+    global tool_config
+    ssh_parameter = {
+        'device_type': switch_element.os,
+        'host': switch_element.ip,
+        'username': tool_config.ssh_username,
+        'password': tool_config.ssh_password,
+        'port': tool_config.ssh_port
+    }
+
+    raw_cli_output = ""
+    try:
+        with ConnectHandler(**ssh_parameter) as ssh_connection:
+            raw_cli_output = ssh_connection.send_command(command)
+    except AuthenticationException:
+        logging.warning(f"Authentication failed for {switch_element.ip}")
+    except NetMikoTimeoutException:
+        logging.warning(f"SSH timeout for {switch_element.ip}")
+    except SSHException:
+        logging.warning(f"SSH not enabled or could not be negotiated for {switch_element.ip}")
+    except Exception:
+        traceback.print_exc()
+        quit()
+
+    return raw_cli_output
+
+
+# Worker thread for fetching ssh config from switch
+def worker(stop_event, input_queue, output_queue, bar):
     while not (stop_event.is_set() and input_queue.empty()):
         try:
-            ssh_parameter, command = input_queue.get(block=True, timeout=1)
+            switch_element, command = input_queue.get(block=True, timeout=1)
         except Empty:
             continue
-        captured_cli_output = {}
-        try:
-            with ConnectHandler(**ssh_parameter) as ssh_connection:
-                raw_cli_output = ssh_connection.send_command(command)
-                captured_cli_output = parse_show_run(ssh_parameter, raw_cli_output)
-                if captured_cli_output is not None:
-                    converted_cli_output = convert_captured_output_to_dict(ssh_parameter, captured_cli_output) #####
-                # captured_cli_output = parse_output(platform="cisco_ios", command="show vlan", data=raw_cli_output)
-        except AuthenticationException:
-            captured_cli_output = {"status": [False, "authentication failed"]}
-            logging.warning(f"Authentication failed for {ssh_parameter['host']}")
-        except NetMikoTimeoutException:
-            captured_cli_output = {"status": [False, "timeout"]}
-            logging.warning(f"SSH timeout for {ssh_parameter['host']}")
-        except SSHException:
-            captured_cli_output = {"status": [False, "not enabled / not negotiated"]}
-            logging.warning(f"SSH not enabled or could not be negotiated for {ssh_parameter['host']}")
-        except Exception:
-            traceback.print_exc()
-            quit()
-        output_queue.put(converted_cli_output) ####
+
+        if switch_element.reachable:
+            raw_cli_output = fetch_switch_config(switch_element, command)
+            switch_element.parse_cli_output(raw_cli_output)
+
+        output_queue.put(switch_element)
         bar()
 
 
-def parse_show_run(ssh_parameter, raw_cli_output):
-    cli_output = {}
-    if raw_cli_output is None:
-        cli_output[ssh_parameter["host"]] = {"status": [False, "did not receive cli output"]}
-        logging.warning(f"{ssh_parameter['host']} - Did not receive CLI output.")
-        return cli_output
-    print("raw:",raw_cli_output)
-    re_pattern = re.compile(r"^(interface.*)\n((?:.*\n)+?)!", re.MULTILINE)  # there is a bug when regex is not matching. Issue #16
-    cli_output = re.findall(re_pattern, raw_cli_output)
-    print("parsed:",cli_output)
-    if cli_output is None:
-        cli_output[ssh_parameter["host"]] = {"status": [False, "regex capture didnt match anything"]}
-        logging.warning(f"{ssh_parameter['host']} - RegEx capture didnt match anything.")
-        return cli_output
-    # cli_output[ssh_parameter["host"]] = {"status": [True, "succeeded"]}
-    return cli_output
-
-
-def convert_captured_output_to_dict(ssh_parameter, captured_cli_output):
-    parsed_interface_data = {}
-    final_cli_output = {}
-    for element in captured_cli_output:
-        interface_name = element[0]
-        interface_config = element[1]
-        interface_config_list = interface_config.split("\n ")
-        interface_config_list = [x.strip() for x in interface_config_list]  # Remove leading blanks
-        parsed_interface_data[interface_name] = interface_config_list
-        final_cli_output[ssh_parameter["host"]] = parsed_interface_data
-    # final_cli_output[ssh_parameter["host"]]["status"] = [True, "succeeded"]
-    return final_cli_output
+def create_json_file(parsed_cli_output):
+    local_time = datetime.now()
+    timestamp_url_safe = (local_time.strftime("%Y-%m-%dT%H-%M-%S"))
+    file_path = "raw_output/show_derived_config_interfaces/" + timestamp_url_safe + ".json"
+    try:
+        with open(file_path, "x") as json_file:
+            json.dump(parsed_cli_output, json_file, indent=2)
+            logging.info(f"Created result file @ {file_path}")
+    except Exception:
+        logging.error("Could not create result file")
+    return
 
 
 def wrapper_send_show_command_to_switches(switch_data, cli_show_command, global_config):
-    ssh_parameter_list = construct_ssh_parameter(switch_data, global_config)
-    unparsed_cli_output = fill_input_queue_start_worker_fill_output_queue(ssh_parameter_list, cli_show_command, global_config)
-    return unparsed_cli_output
-    #print("wrapper output:", unparsed_cli_output)
+    global tool_config
+    tool_config = global_config
+
+    cli_show_command = "show derived-config | begin interface"
+    parsed_cli_output = fill_input_queue_start_worker_fill_output_queue(switch_data, cli_show_command, global_config)
+    create_json_file(parsed_cli_output)
+    return parsed_cli_output
 
     # vlan_parsed = parse_output(platform="cisco_ios", command="show vlan", data=vlan_output)
     # json_vlan_parsed = (json.dumps(vlan_parsed))
