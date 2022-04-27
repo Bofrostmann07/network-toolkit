5c3024dc05ac7b3c30a6b430b916f28fece44926
Bofrostmann07 <84816381+Bofrostmann07@users.noreply.github.com>
2022-04-27 23:17:28 +0200

    feat: load config for different input sources

--- network_toolkit/config/__init__.py
+++ network_toolkit/config/__init__.py
@@ -1,3 +1,4 @@
-from .config import load_config, GLOBAL_CONFIG
+from .global_config import load_global_config, GLOBAL_CONFIG
+from .input_source_config import load_input_config
 
-__all__ = ["load_config", "GLOBAL_CONFIG"]
+__all__ = ["load_global_config", "GLOBAL_CONFIG", "load_input_config"]
